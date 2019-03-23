50e97480223a723d25241a467a51d54d493725d4
Karen Kepner <kkepner14@gmail.com>
2019-03-23 17:44:39 -0600

    Learn how to get the last letter of a string

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// Learn how to get the last letter of a string
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
