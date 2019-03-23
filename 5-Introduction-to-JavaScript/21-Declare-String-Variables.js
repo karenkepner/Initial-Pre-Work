87abe839514680610296631eb8d5ebe8360c7c44
Karen Kepner <kkepner14@gmail.com>
2019-03-23 09:31:10 -0600

    Learn to Declare String Variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+// Learn to Declare String Variables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Karen";
+var myLastName = "Kepner";
