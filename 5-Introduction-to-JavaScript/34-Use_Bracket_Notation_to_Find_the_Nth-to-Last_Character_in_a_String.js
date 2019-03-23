9a15e17361f914769fa27938f1a2f7966a536665
Karen Kepner <kkepner14@gmail.com>
2019-03-23 17:46:23 -0600

    Practice finding different letters in a string

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// Practice finding different letters in a string
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
