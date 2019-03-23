ed4cc820a2cdcc57dd9ecb10b70b7d7326f8a3e0
Karen Kepner <kkepner14@gmail.com>
2019-03-23 17:50:17 -0600

    Learn about basic javascript

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+// Learn about basic javascript
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = `We drove a ${myNoun} as ${myAdjective} as we cound find. Then we ${myVerb} and ${myAdverb}`;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
