ef2607e2c50aeb2f66399c0d09a6dcb7c03a5beb
Karen Kepner <kkepner14@gmail.com>
2019-03-23 17:53:31 -0600

    Learn nested arrays

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+// Learn nested arrays
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [['pizza', 33], ['pie', 9]];
