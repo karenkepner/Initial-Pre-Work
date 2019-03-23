fd7fc0bf7cb5ac7d3b4d879cc4ccbe6433087fed
Karen Kepner <kkepner14@gmail.com>
2019-03-23 17:41:21 -0600

    Learn that strings are immutable, but you can reassign the var to another value

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// Learn that strings are immutable, but you can reassign the var to another value
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
