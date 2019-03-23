d96ed6d75583170d31d36aaadc22ed7c4b0a4b94
Karen Kepner <kkepner14@gmail.com>
2019-03-23 09:46:31 -0600

    Learn constructing strings with variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// Learn constructing strings with variables
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Karen";
+var myStr = "My name is " + myName + " and I am well!";
