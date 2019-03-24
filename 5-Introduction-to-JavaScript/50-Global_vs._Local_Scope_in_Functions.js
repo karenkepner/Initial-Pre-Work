757d236e3c2205a07fa7266808c8c10555db0665
Karen Kepner <kkepner14@gmail.com>
2019-03-23 21:07:24 -0600

    Learn more about scope

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+// Learn more about scope
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = 'sweater';
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
