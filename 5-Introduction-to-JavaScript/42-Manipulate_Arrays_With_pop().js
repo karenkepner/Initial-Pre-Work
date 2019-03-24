c4a78152f56809b3585f3abcd5e58e8ae850cb30
Karen Kepner <kkepner14@gmail.com>
2019-03-23 18:02:03 -0600

    Pop the last item off of an array and assign it to a var

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,11 @@
+// Pop the last item off of an array and assign it to a var
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
