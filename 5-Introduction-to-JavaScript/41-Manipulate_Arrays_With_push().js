0c578d2565104de6cf111e2494721864616fbee2
Karen Kepner <kkepner14@gmail.com>
2019-03-23 18:00:23 -0600

    Learn how to push items into an array at the end of the array

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,13 @@
+// Learn how to push items into an array at the end of the array
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
+
+console.log(myArray);
