4ac53ec3e8d571f2392c4862aced5ab8da3b485a
Karen Kepner <kkepner14@gmail.com>
2019-03-23 20:57:01 -0600

    Learn to write a reusable function

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+// Learn to write a reusable function
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log('Hi World');
+}
+
+reusableFunction();
