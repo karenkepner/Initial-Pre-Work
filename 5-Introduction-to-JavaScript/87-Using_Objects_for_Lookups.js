2c579aa94dd31ce1aa7ef7bfd610a3d390e2551e
Karen Kepner <kkepner14@gmail.com>
2019-03-25 12:26:16 -0600

    Learn how to use an object for lookups

---
+++ 5-Introduction-to-JavaScript/87-Using_Objects_for_Lookups.js
@@ -0,0 +1,21 @@
+// Learn how to use an object for lookups
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
