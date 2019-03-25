a20813439fa4986dde6f7a7b8156addd5e0f326d
Karen Kepner <kkepner14@gmail.com>
2019-03-25 12:48:05 -0600

    Learn how to add items to an object

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// Learn how to add items to an object
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+  "artist": "Brandie Carlile",
+  "title": "By The Way, I Forgive You",
+  "release_year": 2018,
+  "formats": [
+    "CD",
+    "MP3",
+    "LP"
+  ],
+  "gold": false
+  }
+];
