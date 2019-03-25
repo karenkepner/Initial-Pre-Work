8adccd9f9f1db53fe52922970a26c9c664888adf
Karen Kepner <kkepner14@gmail.com>
2019-03-25 12:51:12 -0600

    Learn how to access nested objects

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// Learn how to access nested objects
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+console.log(gloveBoxContents);
