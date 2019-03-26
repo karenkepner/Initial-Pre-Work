b288318b92d11dcbfeb990fa35e38dafe9bd9f30
Karen Kepner <kkepner14@gmail.com>
2019-03-25 18:21:28 -0600

    Learn for loops countdown to 0

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,15 @@
+// Learn for loops countdown to 0
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (let i = 9; i > 0; i -=2) {
+  myArray.push(i);
+}
