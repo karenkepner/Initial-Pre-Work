bf0e5d4e3640fda8441836426e6a2187e1cd2ef7
Karen Kepner <kkepner14@gmail.com>
2019-03-25 20:25:56 -0600

    Learn how to get random whole numbers in a range

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,9 @@
+// Learn how to get random whole numbers in a range
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10) ;
+}
