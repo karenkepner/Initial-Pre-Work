28836398b661a4bb38f1faf40937b9aa972a283f
Karen Kepner <kkepner14@gmail.com>
2019-03-26 08:49:46 -0600

    Learn higher order arrow functions

---
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -0,0 +1,13 @@
+// Learn higher order arrow functions
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  arr = arr.filter( (element) => Number.isInteger(element) && element > 0).map((element) => element * element );
+  const squaredIntegers = arr;
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
