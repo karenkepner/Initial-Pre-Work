577d2eeb95f528d1047b02aa02b64adc75f8329a
Karen Kepner <kkepner14@gmail.com>
2019-03-25 20:48:48 -0600

    Learn the difference between ver and let

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+Learn the difference between ver and let
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
