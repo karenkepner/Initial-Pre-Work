91466c096dcb129ecb76b50a49740fce79047558
Karen Kepner <kkepner14@gmail.com>
2019-03-25 18:16:39 -0600

    Learn for loops

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+// Learn for loops
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (let i = 1; i < 6; i++) {
+    myArray.push(i);
+}
