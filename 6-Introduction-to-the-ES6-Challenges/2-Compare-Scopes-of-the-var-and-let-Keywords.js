02ffad742203c5252372bbe768cdd8ce43f7db43
Karen Kepner <kkepner14@gmail.com>
2019-03-25 21:13:49 -0600

    Learn scope differences between let and var

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+// Learn scope differences between let and var
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
