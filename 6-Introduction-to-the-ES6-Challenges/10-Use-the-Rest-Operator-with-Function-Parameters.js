dbbc28420c4dda72f9dc94e4c1709fe65e667673
Karen Kepner <kkepner14@gmail.com>
2019-03-26 09:21:37 -0600

    Learn the rest operator

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,8 @@
+// Learn the rest operator
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
